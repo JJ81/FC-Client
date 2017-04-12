69fd07f071bcbe4fdf6a228df9fcef43abc9f685
ParkSeokje <molla4455@gmail.com>
2017-04-12 14:14:48 +0900

    테마 red skin 추가

--- util/util.js
+++ util/util.js
@@ -2,29 +2,30 @@
 exports.getLogoInfo = (req, res, next) => {
   let logoImageName;
   let logoName;
+  let theme;
 
   switch (req.headers.host) {
   case 'm.vodaeyewear.orangenamu.net':
     logoImageName = 'vodaeyewear.png';
     logoName = '보다안경원';
+    theme = 'skin-green-light';
     break;
   case 'm.waffle.kosc.orangenamu.net':
     logoImageName = 'waffle.kosc.png';
     logoName = '와플대학';
+    theme = 'skin-red-light';
     break;
   default:
     logoImageName = 'orangenamu.png';
     logoName = '오렌지나무시스템';
+    theme = 'skin-yellow-light';
     break;
   }
   res.locals.logoImageName = logoImageName;
   res.locals.logoName = logoName;
+  res.locals.theme = theme;
 
   return next();
-  // return {
-  //   logoImageName: logoImageName,
-  //   logoName: logoName
-  // };
 };
 
 exports.isAuthenticated = (req, res, next) => {
