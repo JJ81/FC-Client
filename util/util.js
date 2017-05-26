6c071ad44febde4eca668426dca32d02a44071f2
ParkSeokje <molla4455@gmail.com>
2017-05-26 12:21:16 +0900

    도메인 변경에 따른 변경사항 수정

--- util/util.js
+++ util/util.js
@@ -5,16 +5,17 @@ exports.getLogoInfo = (req, res, next) => {
   let theme;
 
   switch (req.headers.host) {
-  case 'm.vodaeyewear.orangenamu.net':
-    logoImageName = 'vodaeyewear.png';
-    logoName = '보다안경원';
-    theme = 'skin-green-light';
-    break;
-  case 'm.waffle.kosc.orangenamu.net':
+  // case 'm.vodaeyewear.orangenamu.net':
+  //   logoImageName = 'vodaeyewear.png';
+  //   logoName = '보다안경원';
+  //   theme = 'skin-green-light';
+  //   break;
+  case 'm.waffle.edu1004.kr':
     logoImageName = 'waffle.kosc.png';
     logoName = '와플대학';
     theme = 'skin-red-light';
     break;
+  case 'm.dev.edu1004.kr':
   default:
     logoImageName = 'orangenamu.png';
     logoName = '오렌지나무시스템';
