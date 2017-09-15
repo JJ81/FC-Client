82043c271793c2e48e99edbc959ce651d6d02653
ParkSeokje <molla4455@gmail.com>
2017-09-15 17:00:26 +0900

    aquaplayer 적용중..

--- dist/javascripts/video_aqua.js
+++ dist/javascripts/video_aqua.js
@@ -1,2 +1,2 @@
-"use strict";window.requirejs(["jquery","axios","aquaNManagerService"],function(e,r,a){e(".videoplayer"),e(function(){var e={videoUrl:"cdnetworks.mp4"};a=new a(e),a.startPlayer()})});
+"use strict";window.requirejs(["jquery","axios","aquaNManagerService"],function(e,r,i){var n=(e(".videoplayer"),e("#btn_play_video"));e(function(){var e={videoUrl:"cdnetworks.mp4"};i=new i(e)}),n.on("click",function(){i.startPlayer()})});
 //# sourceMappingURL=../maps/video_aqua.js.map
--- public/javascripts/video_aqua.js
+++ public/javascripts/video_aqua.js
@@ -6,12 +6,16 @@ window.requirejs(
   ],
 function ($, axios, AquaNManagerService) {
   var playerContainer = $('.videoplayer');
+  var btnPlayVideo = $('#btn_play_video');
 
   $(function () {
     var options = {
       videoUrl: 'cdnetworks.mp4' // playerContainer.data('url')
     };
     AquaNManagerService = new AquaNManagerService(options);
+  });
+
+  btnPlayVideo.on('click', function () {
     AquaNManagerService.startPlayer();
   });
 });
