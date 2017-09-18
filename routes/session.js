9b5a669efdcf70c88294992adf2eb941b78585d9
ParkSeokje <molla4455@gmail.com>
2017-09-18 20:41:49 +0900

    aquaplayer 적용중..

--- routes/session.js
+++ routes/session.js
@@ -169,7 +169,7 @@ router.get('/:training_user_id/:course_id/:course_list_id', util.isAuthenticated
  * AquaPlayer의 return url 요청을 처리할 route
  */
 router.get('/player/check/:training_user_id/:course_id/:course_list_id/:video_id', (req, res, next) => {
-  console.log(req.query);
+  console.log(req.user, req.params);
   res.sendStatus(200);
 });
 
