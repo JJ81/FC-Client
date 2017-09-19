9e978757104139688a41f67f12ebc9cf10ab3651
ParkSeokje <molla4455@gmail.com>
2017-09-19 16:00:38 +0900

    aquaplayer 적용중..

--- public/javascripts/video_aqua.js
+++ public/javascripts/video_aqua.js
@@ -36,13 +36,12 @@ function ($, axios, AquaNManagerService, Timer) {
       setTimeout(function () {
         // timerWait = $.timer(1000 * 1, waitingTimeLogger, true);
         var timer = new Timer();
-        var message = timer.getTimeValues().toString() + ' 후 학습이 초기화 됩니다. 다음 버튼을 눌러주세요.';
         timer.start({countdown: true, startValues: {seconds: 30}});
 
-        waitMessage.html(message);
+        waitMessage.html(timer.getTimeValues().toString() + ' 후 학습 초기화');
 
         timer.addEventListener('secondsUpdated', function (e) {
-          waitMessage.html(message);
+          waitMessage.html(timer.getTimeValues().toString()) + ' 후 학습 초기화';
         });
 
         timer.addEventListener('targetAchieved', function (e) {
